ade7a8edbf3c0cf36649a65fb70517e83e67057e
hfavereau <harold.favereau@etudiant.univ-rennes1.fr>
2023-01-04 12:17:07 +0100

    test

---
+++ src/main/java/fr/oci/onlyfriendsin/onlyfriendsin/OnlyfriendsinApplication.java
@@ -0,0 +1,13 @@
+package fr.oci.onlyfriendsin.onlyfriendsin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyfriendsinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyfriendsinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/oci/onlyfriendsin/onlyfriendsin/OnlyfriendsinApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.oci.onlyfriendsin.onlyfriendsin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyfriendsinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
