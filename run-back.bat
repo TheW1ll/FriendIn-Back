0b36ee610965e26034d76f27677273bf5d4042e8
hfavereau <harold.favereau@etudiant.univ-rennes1.fr>
2023-01-16 14:35:57 +0100

    correction script

--- run-back.bat
+++ run-back.bat
@@ -2,3 +2,5 @@ cd hsqldb
 start cmd /c java -cp hsqldb.jar org.hsqldb.Server
 cd ..
 start cmd /c gradlew.bat "-Dorg.gradle.java.home=\home\junkyards\.jdks\corretto-17.0.5" "bootRun"
+echo Press any key to exit . . .
+pause>nul
